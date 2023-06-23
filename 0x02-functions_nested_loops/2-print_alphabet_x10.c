16faf0a8d23c17d2a4eb668e24b2669317fdbe17
abdulnasirym <abdulnasiry03@gmail.com>
2023-06-23 01:40:36 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,5 +1,5 @@
 #include "main.h"
-/** 
+/**
  * print_alphabet
  *
  * Return: 0
@@ -14,5 +14,5 @@ void print_alphabet_x10(void)
 		for (j = 'a' ; j <= 'z' ; j++)
 			_putchar(j);
 		_putchar('\n');
-	}	
+	}
 }
