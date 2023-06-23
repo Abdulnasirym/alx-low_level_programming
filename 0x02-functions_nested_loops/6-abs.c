9a61523990798fde2be5c5d5e3355cb0cc393944
abdulnasirym <abdulnasiry03@gmail.com>
2023-06-23 15:34:50 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - function that finds the absolute value of a number
+ * @n: function parameer
+ * Return: 0
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	else
+		return (n);
+}
