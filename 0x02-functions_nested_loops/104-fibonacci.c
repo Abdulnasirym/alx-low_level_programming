99825f38231269c593f36d79202d1b28a6263de4
abdulnasirym <abdulnasiry03@gmail.com>
2023-06-25 16:11:03 -0700

    task 16

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Return: 0
+ */
+
+int main(void)
+{
+	unsigned int limit = 98;
+	unsigned int term1 = 1;
+	unsigned int term2 = 2;
+	unsigned int count = 2;
+	unsigned int next;
+
+	printf("%u, %u", term1, term2);
+	while (count <= limit)
+	{
+		next = term1 + term2;
+		printf(", %u", next);
+		term1 = term2;
+		term2 = next;
+		count++;
+	}
+	printf("\n");
+	return (0);
+}
