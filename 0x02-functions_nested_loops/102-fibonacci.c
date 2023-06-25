e6af35abe2eec8fa67d029a1316de1a7de036dff
abdulnasirym <abdulnasiry03@gmail.com>
2023-06-25 14:18:23 -0700

    task 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - function that prints first 50 fibonacci numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	unsigned long int next, first = 1, second = 2;
+
+	printf("%lu", first);
+	for (i = 1 ; i <= 50 ; i++)
+	{
+		printf("%lu", second);
+		next = first + second;
+		first = second;
+		second = next;
+		if (i != 50)
+			printf(", ");
+	}
+	printf("\n");
+	return (0);
+}
