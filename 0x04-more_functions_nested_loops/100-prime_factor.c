5cba8609be24fe33d2d9e41c6ea4f9a60fc6e137
abdulnasirym <abdulnasiry03@gmail.com>
2023-06-27 17:02:44 -0700

    task 11

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Return: 0
+ */
+
+int main(void)
+{
+	long int x = 612852475143;
+	long int y;
+
+	for (y = 2 ; y <= x ; y++)
+	{
+		if (x% y == 0)
+		{
+			x = x / y;
+			y--;
+		}
+	}
+	printf("%lu\n", y);
+	return (0);
+}
