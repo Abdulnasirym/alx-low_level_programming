25832da71a7994ec59b0c62ba0274a8282fc9047
abdulnasirym <abdulnasiry03@gmail.com>
2023-06-30 07:27:23 -0700

    task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,15 +8,13 @@
 
 void rev_string(char *s)
 {
-	int i;
+	int i, j;
 	int count = 0;
 
 	for (i = 0 ; s[i] != '\0' ; i++)
 		count++;
 	for (i = 0 ; i < count / 2 ; i++)
 	{
-		int j;
-
 		j = s[i];
 		s[i] =  s[count - 1 - i];
 		a[count - 1 - i] = j;
