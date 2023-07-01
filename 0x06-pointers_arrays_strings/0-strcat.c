567cd90179afe8db4a54dc96e2681365a2a75beb
abdulnasirym <abdulnasiry03@gmail.com>
2023-07-01 04:19:24 -0700

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -16,6 +16,8 @@ char *_strcat(char *dest, char *src)
 	for (i = 0 ; dest[i] != '\0' ; i++)
 		destlen++;
 	for (i = 0 ; src[i] != '\0' ; i++)
+		srclen++;
+	for (i = 0 ; src[i] != '\0' ; i++)
 		dest[destlen + i] = src[i];
 	return (dest);
 }
