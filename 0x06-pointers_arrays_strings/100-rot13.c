5442a1afb385f35a8e0c8e744c86fb3723c31ea5
abdulnasirym <abdulnasiry03@gmail.com>
2023-07-01 15:30:56 -0700

    task 8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rot13 - rot13 encoding function
+ * @s: string to encode
+ * Return: encoded string
+ */
+
+char *rot13(char *s)
+{
+	int i, j;
+	char *a = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char *b = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	
+	for (i = 0 ; s[i] != '\0' ; i++)
+	{
+		for (j = 0 ; a[j] != '\0' ; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
