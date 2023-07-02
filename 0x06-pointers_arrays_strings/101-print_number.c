6dcb269716d4e9a515858d2b94d482fb608d1ea3
abdulnasirym <abdulnasiry03@gmail.com>
2023-07-02 00:07:12 -0700

    task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,14 +8,16 @@
 
 void print_number(int n)
 {
+	int m;
+
 	if (n < 0)
 	{
-		n = -n;
+		m = -n;
 		_putchar('-');
 	}
 	else
-		n = n;
-	if (n / 10)
-		print_number(n / 10);
-	_putchar((n % 10) + '0');
+		m = n;
+	if (m / 10)
+		print_number(m / 10);
+	_putchar((m % 10) + '0');
 }
