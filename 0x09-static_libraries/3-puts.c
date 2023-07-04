e23c08df90645c7b43efe86ee470dbb855f0f581
abdulnasirym <abdulnasiry03@gmail.com>
2023-07-04 14:12:57 -0700

    task 0

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -6,11 +6,11 @@
  * Return: void
  */
 
-void _puts(char *s)
+void _puts(char *str)
 {
 	int i;
 
-	for (i = 0 ; s[i] != '\0' ; i++)
-		_putchar(s[i]);
+	for (i = 0 ; str[i] != '\0' ; i++)
+		_putchar(str[i]);
 	_putchar('\n');
 }
