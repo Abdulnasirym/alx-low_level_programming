466a20d2da911ccd663bfd15fc9b1351aa307e68
abdulnasirym <abdulnasiry03@gmail.com>
2023-07-04 12:59:15 -0700

    task 0

---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strspn - get the length of a prefix sub string
+ * @s: string to search from
+ * @accept: bytes to search for
+ * Return: length of the prefix sub string
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+	{
+		for (j = 0 ; s[i] != accept[j] ; j++)
+		{
+			if (accept[j] == '\0')
+				return (i);
+		}
+	}
+	return (0);
+}
