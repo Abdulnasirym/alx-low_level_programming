20de62aa2b5f227392238f99e02ed896b9621b44
abdulnasirym <abdulnasiry03@gmail.com>
2023-07-05 02:00:09 -0700

    task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 /**
  * main - adds positie numbers
@@ -20,7 +21,7 @@ int main(int argc, char *argv[])
 		{
 			for (j = 0 ; argv[i][j] != '\0' ; j++)
 			{
-				if (!(atoi(argv[i])))
+				if (!(isdigit(argv[i][j])))
 				{
 					printf("Error\n");
 					return (1);
