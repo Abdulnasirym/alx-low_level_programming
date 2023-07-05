2717e36410a9fbc4d8cf76d892fc3d4c600c5005
abdulnasirym <abdulnasiry03@gmail.com>
2023-07-05 02:10:53 -0700

    task 5

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -13,12 +13,14 @@ int main(int argc,char *argv[])
 	int cents;
 	int coins = 0;
 
-	if (argc == 1 || argc > 2)
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
+
 	cents = atoi(argv[1]);
+
 	while (cents > 0)
 	{
 		if (cents >= 25)
