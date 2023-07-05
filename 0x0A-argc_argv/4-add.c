052cfcef8d52bccc82b6d3dce3ec9f6e4b104d0d
abdulnasirym <abdulnasiry03@gmail.com>
2023-07-05 01:29:36 -0700

    task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - adds positie numbers
+ * @argc: argumnets count
+ * @argv: argument vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	if (argc == 1)
+		printf("%d\n", 0);
+	else
+	{
+		for (i = 1 ; i < argc ; i++)
+		{
+			if (!(atoi(argv[i])))
+			{
+				printf("Error\n");
+				return (1);
+			}
+			else
+				sum = sum + argv[i];
+		}
+		printf("%d\n", sum);
+	}
+	return (0);
+}
