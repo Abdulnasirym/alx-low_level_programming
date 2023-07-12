1d6a993554c094502abff4ace338f8a1905048df
abdulnasirym <abdulnasiry03@gmail.com>
2023-07-12 11:36:14 -0700

    task 3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,71 @@
+#include "3-calc.h"
+
+/**
+ * op_add - add two parameters
+ * @a: para 1
+ * @b: param 2
+ * Return: sum;
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtract two parameters
+ * @a: para 1
+ * @b: param 2
+ * Return: sub;
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies two parameters
+ * @a: para 1
+ * @b: param 2
+ * Return: product;
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divide two parameters
+ * @a: para 1
+ * @b: param 2
+ * Return: divide;
+ */
+
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
+}
+
+/**
+ * op_mod - modulus
+ * @a: para 1
+ * @b: param 2
+ * Return: sum;
+ */
+
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}
