c65c85c87852e0ab1cd08107871df282c15038b6
abdulnasirym <abdulnasiry03@gmail.com>
2023-07-12 15:34:22 -0700

    task 3

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,50 +1,48 @@
 #include "3-calc.h"
-#include <stdlib.h>
-#include <stdio.h>
 
 /**
- * op_add - ...
- * @a: ...
- * @b: ...
- *
- * Return: ...
+ * op_add - add two parameters
+ * @a: para 1
+ * @b: param 2
+ * Return: sum;
  */
+
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 
 /**
- * op_sub - ...
- * @a: ...
- * @b: ...
- *
- * Return: ...
+ * op_sub - subtract two parameters
+ * @a: para 1
+ * @b: param 2
+ * Return: sub;
  */
+
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 
 /**
- * op_mul - ...
- * @a: ...
- * @b: ...
- *
- *  Return: ...
+ * op_mul - multiplies two parameters
+ * @a: para 1
+ * @b: param 2
+ * Return: product;
  */
+
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 
 /**
- * op_div - ...
- * @a: ...
- * @b: ...
- *
- *  Return: ...
+ * op_div - divide two parameters
+ * @a: para 1
+ * @b: param 2
+ * Return: divide;
  */
+
 int op_div(int a, int b)
 {
 	if (b == 0)
@@ -52,17 +50,16 @@ int op_div(int a, int b)
 		printf("Error\n");
 		exit(100);
 	}
-
 	return (a / b);
 }
 
 /**
- * op_mod - ...
- * @a: ...
- * @b: ...
- *
- * Return: ...
+ * op_mod - modulus
+ * @a: para 1
+ * @b: param 2
+ * Return: division;
  */
+
 int op_mod(int a, int b)
 {
 	if (b == 0)
@@ -70,6 +67,5 @@ int op_mod(int a, int b)
 		printf("Error\n");
 		exit(100);
 	}
-
 	return (a % b);
 }
