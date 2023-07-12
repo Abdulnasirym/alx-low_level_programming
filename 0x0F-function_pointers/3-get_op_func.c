3f494e0ff632e6ca0eb9976a57da2a0486d1b1af
abdulnasirym <abdulnasiry03@gmail.com>
2023-07-12 12:08:16 -0700

    task 3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -5,7 +5,7 @@
  * get_op_func - get operation to be used on parameter passed
  * @s: function parameter
  * Return: the operator
- */
+g*/
 
 int (*get_op_func(char *s))(int, int)
 {
@@ -21,7 +21,7 @@ int (*get_op_func(char *s))(int, int)
 
 	i = 0;
 
-	while (i < 5)
+	while (*ops[i].op)
 	{
 		if (*s == *ops[i].op)
 			return (*ops[i].f);
