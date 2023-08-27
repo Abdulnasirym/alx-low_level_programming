5220f84d6848fb3f2f20839e18148231c579e5d7
Abdulnasirym <abdulnasiry03@gmail.com>
2023-08-27 12:21:00 +0000

    task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+* print_function - print string using printf
+*/
+
+void __attribute__((constructor)) print_function(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
